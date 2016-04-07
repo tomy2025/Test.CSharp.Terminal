d29318bc1f74f8ff235fe246d35702d0b8633b70
Тома Баксанов <tomy_@abv.bg>
2016-04-07 21:20:58 +0300

    Създаване на програма Hello everybody

---
+++ HelloEverybody/HelloEverybody/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloEverybody
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello everybody!");
+			Console.ReadKey ();
+		}
+	}
+}
